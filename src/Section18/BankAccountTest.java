a908416e501df0a4969d713a46be4e775bdd92c2
buckhi <101989012+buckhi@users.noreply.github.com>
2022-03-25 00:17:43 +0700

    Add files via upload

---
+++ src/Section18/BankAccountTest.java
@@ -0,0 +1,24 @@
+package Section18;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("this has been implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("this has been implemented");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("this has been implemented");
+
+    }
+
+}
---
+++ src/Section18/BankMain.java
@@ -0,0 +1,7 @@
+package Section18;
+
+public class BankMain {
+    public static void main(String[] args) {
+        System.out.println("the system is running");
+    }
+}
